35d72a3be6d3d9a6ee3eae3b9a3e6d1b5d7aa224
Sheila Wittman <szandora789@gmail.com>
2018-06-09 00:01:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,32 @@
 def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1) do |dwarf, index|
     puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet(planeteers)
-  planeteers.map { |call| call.capitalize + "!"}
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls(planeteers)
-  planeteers.any? do |planeteer|
-+    planeteers.length > 4
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|call| call.length > 4 }
 end
 
-def find_the_cheese
+
+def find_the_cheese(words)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+
   cheese_types.each do |cheese|
-+    if words.include?(cheese)
-+      return cheese
-+    end
-+  end
-+
-+  return nil
+    if words.include?(cheese)
+      return cheese
+    end
+  end
+
+  return nil
 end
+
+
+  
